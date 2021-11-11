02b2dc3a23146ef73036e25103446515da735d23
himanshu06d <hd156681@gmail.com>
2021-11-11 23:03:06 +0530

    integrate maven with jenkins

---
+++ src/main/java/com/upes/upes/secondexp.java
@@ -0,0 +1,11 @@
+package com.upes.upes;
+
+public class secondexp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello maven");
+
+	}
+
+}
